73605afb09846c7df4dbaf66c866c93dc8093779
Taumer <taumer943@gmail.com>
2012-01-31 17:53:17 +0600

    update  SQL

--- sql/Custom/[MC]006_world_spell_script_name.sql
+++ sql/Custom/[MC]006_world_spell_script_name.sql
@@ -1,3 +1,4 @@
+DELETE FROM spell_script_names WHERE spell_ip IN (50365,50371,50391,50392,13191)
 INSERT INTO spell_script_names VALUES
 (50365, "spell_dk_improved_blood_presence"),
 (50371, "spell_dk_improved_blood_presence"),
@@ -5,4 +6,4 @@ INSERT INTO spell_script_names VALUES
 (50392, "spell_dk_improved_unholy_presence"),
 (13161, "spell_hun_aspect_of_the_beast");
 
-DELETE FROM spell_script_names WHERE spell_id in (20911,25899,63944,68066);
+DELETE FROM spell_script_names WHERE spell_id IN (20911,25899,63944,68066);
