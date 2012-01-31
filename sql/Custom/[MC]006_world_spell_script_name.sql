379ef37509809cf305e362abff4042c3ecc7a1d4
vertrigo <admin@uncover-wow.ru>
2012-01-31 20:59:01 +0200

    fix error in SQL

--- sql/Custom/[MC]006_world_spell_script_name.sql
+++ sql/Custom/[MC]006_world_spell_script_name.sql
@@ -1,4 +1,4 @@
-DELETE FROM spell_script_names WHERE spell_ip IN (50365,50371,50391,50392,13191)
+DELETE FROM spell_script_names WHERE spell_id IN (50365,50371,50391,50392,13191);
 INSERT INTO spell_script_names VALUES
 (50365, "spell_dk_improved_blood_presence"),
 (50371, "spell_dk_improved_blood_presence"),
